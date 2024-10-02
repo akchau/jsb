869485867a1ad8827b4e28e18eb47e53d55b883c
ВÐGleb L <gllazrv@gmail.ru>
2024-10-02 10:42:25 +0300

    Добавлена обработка Network Error

--- src/bot/core.py
+++ src/bot/core.py
@@ -2,6 +2,8 @@
 Основной модуль бота.
 """
 import logging
+
+from telegram.error import NetworkError
 from telegram.ext import Application
 
 from src.bot.handlers import main_conv_handler
@@ -17,10 +19,12 @@ logger = logging.getLogger(__name__)
 
 def start_bot() -> None:
     """Run the bot."""
-    application = Application.builder().token(settings.BOT_TOKEN).build()
-    application.add_handler(main_conv_handler)
-    application.run_polling()
-
+    try:
+        application = Application.builder().token(settings.BOT_TOKEN).build()
+        application.add_handler(main_conv_handler)
+        application.run_polling()
+    except NetworkError:
+        logger.error("Не удалось запустить бот. Недоступна сеть!")
 
 if __name__ == "__main__":
     start_bot()
